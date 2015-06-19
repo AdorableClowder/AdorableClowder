3e5769ca6d13b40640b6faa3e647de40372eb94f
Justin Thareja <justin.thareja@gmail.com>
2015-06-19 12:19:11 -0700

    closes #20

--- server/controller.js
+++ server/controller.js
@@ -1,1 +1,19 @@
 // All logic interactions taking info from request and responding from db
+
+
+module.exports = {
+
+  getMatchingUsers = function (req, res, next) {
+    res.send()
+  },
+
+  createUser = function () {
+
+  },
+
+  logoutUser = function () {
+
+  }
+
+
+}
--- server/router.js
+++ server/router.js
@@ -1,12 +1,12 @@
 var helpers = require('./helpers.js'); // any handy utility function
 var controller = require('./controller.js'); // all actual route handling logic will reside here
-var app = require('./server.js')
+var app = require('./server.js');
 
 // Router
 // TODO: make sure to bounce users to signup if they're not logged in
-app.get('/explore', controller.getUsers); 
+app.get('/explore', controller.getMatchingUsers); 
 app.post('/signup', controller.createUser);
-app.post('/login', controller.getUsers);
+// app.post('/login', send token and user info); 
 app.post('/logout', controller.logoutUser);
 
 
