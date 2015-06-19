388afaa35f5aba54c4cee1f6b6396418af1d61f1
Justin Thareja <justin.thareja@gmail.com>
2015-06-19 14:34:52 -0700

    (init) moved server.js to root
    (init) remove old server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require('express');
+// var morgan = require('morgan');
 
 var app = express();
 var port = process.env.PORT || 1337;
 
 // Configuration for all environments -- will probably need to separate between dev & prod later
-app.configure(function() {
-  app.use(express.static(__dirname + '/client'));
-});
+app.use(express.static(__dirname + '/client'));
 
 app.listen(port);
 
 console.log('server listening on port: ', port);
 
-modules.export = app;
+module.export = app;
