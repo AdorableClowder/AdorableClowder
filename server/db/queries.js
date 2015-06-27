6b6c61f0c7dda69745623ac6dc623aaa804ca8f3
Justin Thareja <justin.thareja@gmail.com>
2015-06-26 20:09:51 -0700

    (refactor) flatten createUser function a bit

--- server/db/queries.js
+++ server/db/queries.js
@@ -7,29 +7,27 @@ var Offer = Models.Offer;
 var Want = Models.Want;
 
 
-// a function that takes the agreed-upon json user-obj posted to '/signup' (see docs/interface.json for more info)
-// adds the user to the database and establishes a link between wanted and offered skills
-var createUser = exports.createUser = function (jsonUser, next) {
-  var user = jsonUser;
-
-  var newUser = User.forge({
+// createUser is a promise that takes a user-obj posted to '/signup' (see docs/interface.json for more info) 
+// and a next callback. createUser adds the user to the database and establishes a link between wanted and offered skills
+var createUser = exports.createUser = function (user, next) {
+  
+  return User.forge({
     username: user.username
-  });
-
-  return newUser
+  })
     .fetch()
     .then(function (userExists) {
       if (userExists) {
         throw new Error('User already exists!');
       }
-      console.log("about to hash")
-      newUser.set("email", user.email);
-      console.log('newUser: ', newUser);
-      console.log('user: ', user)
+      return User.forge({
+        username: user.username,
+        email: user.email
+      });
+    })
+    .then(function (newUser) {
       return newUser.hashPassword(user.password);
     })
     .then(function (savedUser) {
-      console.log('savedUser: ', savedUser)
       var relationships = [
         attachSkillsToUser(savedUser, user.offer, 'offers'),
         attachSkillsToUser(savedUser, user.want, 'wants')
@@ -42,24 +40,26 @@ var createUser = exports.createUser = function (jsonUser, next) {
     .catch(function (error) {
       next(error);
     });
+
 };
 
-// attachSkillsToUser is a promise that takes a backbone User model, an array of skills (as strings),
+// attachSkillsToUser is a promise that takes a Bookshelf User model, an array of skills (as strings),
 // and the corresponding table the skills belong to (eg: 'offers' or 'wants') and creates the Bookstrap version
 // of a join table between the user and the skills on that table
 var attachSkillsToUser = function (user, skills, table) {
   return new Promise(function (resolve, reject) {
     getAllSkillIds(skills, convertToModelName(table)).then(function (ids) {
-      user.related(table).attach(ids); //populates join table
-      console.log('successfully attached skills to', table, 'table');
+      // attaches an array of ids from from the table passed into the 'related' method
+      user.related(table).attach(ids);
+      console.log('successfully attached skills to', table);
       resolve(user);
     });
   });
 };
 
 /*
-getAllSkillIds is a promise that accepts an array of skill strings, and a string representing a
-Backbone skill type model and adds them to the db if not already there. an array of id's
+getAllSkillIds is a promise that accepts an array of skill strings and a string representing a
+Bookshelf skill type model, and adds them to the db if not already there. an array of id's
 associated with the corresponding skill are passed to the resolve function for .then chaining
 
 ex: getAllSkillIds(['skyrim', 'yoga', 'surfing'], 'Want').then(function (ids) {
@@ -71,62 +71,40 @@ var getAllSkillIds = function (skills, skillType) {
     return getSkillId(skill, skillType);
   }));
 };
-// var getAllSkillIds = function (skills, skillType) {
-//   return new Promise (function (resolve, reject) {
 
-//     var newSkillIds = [];
-
-//     async.each(skills, function (skill, callback) {
-//       console.log('getting skill ID for', skill);
-//       getSkillId(skill, skillType).then(function(skillId) {
-//         newSkillIds.push(skillId);
-//         callback();
-//       });
-
-//     }, function (err) {
-//       if (err) {
-//         console.log('failed to retreive skill id');
-//       } else {
-//         console.log('all new skills added to db');
-//         resolve(newSkillIds);
-//       }
-//     });
-//   });
-// };
-
-
-
-// getSkillId is a promise that accepts a raw skill string (eg: 'cooking') and a string representing a Backbone class
+// getSkillId is a promise that accepts a raw skill string (eg: 'cooking') and a string representing a Bookshelf class
 // and gets the skill's id from the table based on skill type. the found is then passed through the resolve function
 // NOTE: if the skill is not already in the db, getSkillId will add it to the db before resolving
 var getSkillId = function (skill, skillType) {
-  // convert 'skillType' to the actual Backbone class
+  // convert 'skillType' to the actual Bookshelf class
   var Model = Models[skillType];
 
   return new Promise(function (resolve, reject) {
     Model.forge({
       skill: skill
     }).fetch().then(function (skillExists) {
-      if (!skillExists) {
-        Model.forge({
-          skill: skill
-        }).save().then(function (savedSkill) {
-          console.log(savedSkill.get('skill'), 'saved successfully in ' + skillType + 's table');
-          console.log('with an id of:', savedSkill.get('id'));
-          // pass saved skill id into then function
-          resolve(savedSkill.get('id'));
-        });
-      } else {
-        console.log(skill, 'already exists in db');
+      if (skillExists) {
         // pass fetched skill id into then function
         resolve(skillExists.get('id'));
+        return;
       }
+      Model.forge({
+        skill: skill
+      }).save().then(function (savedSkill) {
+        console.log(
+          savedSkill.get('skill'),
+          'saved successfully in ' + skillType + 's table',
+          'with an id of:', savedSkill.get('id')
+        );
+        // pass saved skill id into then function
+        resolve(savedSkill.get('id'));
+      });
     });
   });
 };
 
 
-// useful when distinguishing between the MySQL table name VS. the Backbone model name
+// useful when distinguishing between the MySQL table name VS. the Bookshelf model name
 var convertToModelName = function (tableName) {
   return tableName.charAt(0).toUpperCase() + tableName.slice(1, tableName.length - 1);
 };
