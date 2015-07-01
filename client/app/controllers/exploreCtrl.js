e6aa03d787dd297b7ffd5851a95a2e5fbca4be8b
jseiden <jseiden@gmail.com>
2015-07-01 16:19:30 -0700

    make tally of offered skills

--- client/app/controllers/exploreCtrl.js
+++ client/app/controllers/exploreCtrl.js
@@ -24,14 +24,41 @@ angular.module('exploreCtrl', [])
     var allOffers = _.map(array, function (item) {
       return item.offer;
     });
+
     allOffers = _.flatten(allOffers);
+    // console.log("allOffers: ", allOffers);
     vm.userSkills = _.uniq(allOffers);
+    // console.log("vm.userSkills: ", vm.UserSkills);
     //if there are no skills available, display error message
     if (vm.userSkills.length === 0) {
       vm.err = "Sorry, no users are currently looking for your skills.";
     }
   };
+  // addition by Jake
+  vm.getSkillStats = function(){
 
+    console.log("getSkillStats firred");
+    // get data from users who aren't the current user
+    Users.getOtherUsers(vm.user)
+      .then(function (data){
+        var userInfo = [];
+        // put all the other users' skills offered in one array
+        _.each(data, function(item){
+          userInfo.push( _.flatten(item.offer));
+        });
+        // count the occurences of each skill offered
+        var tally = {};
+        _.each(_.flatten(userInfo), function(el){
+          tally[el] = tally[el] + 1 || 1;
+        });
+        vm.tally = tally;
+        console.log("tally: ", vm.tally);
+      })
+      .catch(function (err){
+        console.log(err);
+      });
+
+  };
   //shows list of skills by default, or people with certain skill when clicked
   vm.shouldShow = true;
   vm.toggleView = function () {
