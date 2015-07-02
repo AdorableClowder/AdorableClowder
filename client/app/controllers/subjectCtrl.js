9da9287dadcdb8988de650c2e8f7d3a679cc96d0
Danielle Blank <dgdblank@gmail.com>
2015-07-02 16:53:54 -0700

    (feat) Progress on choose subjects page, filtering by category

--- client/app/controllers/subjectCtrl.js
+++ client/app/controllers/subjectCtrl.js
@@ -5,15 +5,28 @@ angular.module('subjectCtrl', [])
     vm.user = {};
     vm.wants = [];
     vm.offers = [];
+    vm.wantsObj = {};
+    vm.offersObj = {};
 
     vm.getUser = function () {
       //using Users factory from factories.js to do GET
       Users.getUser()
         .then(function (user) {
+        	console.log(user);
           vm.user = user;
-          vm.wants = vm.user.want;
-          vm.offers = vm.user.offer;
-          console.log(vm.user);
+          vm.wants = user.want;
+          vm.offers = user.offer;
+        })
+        .then(function (user){
+    	   	// Pushes all wants/offers into wants/offers object according to category type 
+    	   	if(vm.wants !== undefined && vm.offers !== undefined){
+    		    for(var j = 0; j < vm.wants.length; j++){
+    		    	vm.wantsObj[vm.wants[j].category].push(vm.wants[j]);
+    		    }
+    		    for(var k = 0; k < vm.offers.length; k++){
+    		    	vm.offersObj[vm.offers[k].category].push(vm.offers[k]);
+    		    }
+    		}
         })
         .catch(function (err) {
           console.log(err);
@@ -21,7 +34,6 @@ angular.module('subjectCtrl', [])
           $location.path('/login');
         });
     };
-    
     vm.getUser();
 
     vm.categories = ['Language Learning', 'Technology', 'Sports', 'Knowledge', 'Wild n Wacky', 'Business', 'Craft and Design'];
@@ -35,7 +47,6 @@ angular.module('subjectCtrl', [])
       'Business': ["Money Laundering", "Accounting", "Financial Advice"],
       'Craft and Design': ["Woodworking", "Clay Pottery", "Graphic Design"]
     };
-
     
     // Selects category from dropdown menu and displays the category to submit
     vm.selectedWantCategory = "category";
@@ -51,20 +62,28 @@ angular.module('subjectCtrl', [])
     vm.chooseOffers = false;
 
     vm.toggleWant = function(want, category) {
+    	console.log("want", want);
+    	console.log("category", category);
       var index = vm.wants.indexOf(want);
-      if (index > -1) {
+      var idx = vm.wantsObj.category.indexOf(want);
+      if (index > -1 || idx > -1) {
         vm.wants.splice(index, 1);
+        vm.wantsObj.category.splice(idx, 1);
       } else {
         vm.wants.push({skill: want, category: category});
+        vm.WantsObj.category.push({skill: want, category: category});
       }
     };
 
     vm.toggleOffer = function(offer, category) {
       var index = vm.offers.indexOf(offer);
-      if (index > -1) {
+      var idx = vm.offersObj.category.indexOf(offer);
+      if (index > -1 || idx > -1) {
         vm.offers.splice(index, 1);
+        vm.offersObj.category.splice(idx, 1);
       } else {
         vm.offers.push({skill: offer, category: category});
+        vm.offersObj.category.push({skill: offer, category: category});
       }
     };
 
@@ -81,4 +100,9 @@ angular.module('subjectCtrl', [])
         });
     };
 
+    // Add categories as keys to the wants and offers objects
+    for(var i = 0; i < vm.categories.length; i++){
+    	vm.wantsObj[vm.categories[i]] = [];
+    	vm.offersObj[vm.categories[j]] = [];
+    }
 });
